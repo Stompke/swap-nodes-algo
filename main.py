aac5d0e370cccce67324ebf85738fe5e9ed39092
Shawn Tompke <shawntompke@gmail.com>
2020-08-14 05:28:13 -0700

    challenge added

---
+++ main.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/swap-nodes-algo/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
+
+"""
+UPER:
+U:
+    >
+P:
+    >
+E: 
+    > CODE! 👨‍💻
+R:
+    > 
+"""
