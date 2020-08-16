208804350f134acae83d9142728fd8ea31de2e53
Shawn Tompke <shawntompke@gmail.com>
2020-08-16 06:23:24 -0700

    tree is swapping, just need to do an in order traversal

--- main.py
+++ main.py
@@ -30,9 +30,11 @@ def swapNodes(indexes, queries):
     root = BSTNode(1)
     q = deque()
     count = 0
+    depth = 1
 
     q.appendleft(root)
 
+    # build the tree
     while len(q) > 0:
         cur = q.pop()
         print(cur.value)
@@ -46,8 +48,64 @@ def swapNodes(indexes, queries):
 
         count += 1 
 
-    print("\t", root.value)
-    print("\t", root.left.value)
-    print("\t", root.right.right.value)
 
-swapNodes([[2, 3], [-1, -1], [-1, -1]],1)
+    # Show the tree
+    print("\t     ", root.value)
+    print("\t", "    / \\")
+    print("\t  ", root.left.value,"   ", root.right.value)
+
+    print("\t", "/  \\"," ", "/  \\")
+    print("      ", root.left.left.value,"  ", root.left.right.value,root.right.left.value,"  ",root.right.right.value)
+
+    # traverse the tree
+    
+    q.appendleft(root)
+    q.appendleft(False)
+
+    print("\n TRAVERSE TREE \n")
+
+    print("\t Depth: ", depth)
+    while len(q) > 1:
+
+        cur = q.pop()
+        if cur == False:
+            q.appendleft(False)
+            depth += 1
+            print("\t Depth: ", depth)
+
+            if not depth % queries[0]:
+                print("\t\t\t It is a mulitple!")
+                print(depth,queries[0])
+
+
+
+            # if depth == queries[0]
+            # if depth == 
+        else:
+            print(cur.value)
+
+            # swap nodes
+            if not depth % queries[0]:
+                cur.left, cur.right = cur.right, cur.left
+            
+            if cur.left.value > 0:
+                q.appendleft(cur.left)
+            if cur.right.value > 0:
+                q.appendleft(cur.right)
+    return root
+
+    # Show the tree
+    print("\t     ", root.value)
+    print("\t", "    / \\")
+    print("\t  ", root.left.value,"   ", root.right.value)
+
+    print("\t", "/  \\"," ", "/  \\")
+    print("      ", root.left.left.value,"  ", root.left.right.value,root.right.left.value,"  ",root.right.right.value)
+
+        
+
+
+indices = [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9], [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]]
+queries = [2,4]
+
+swapNodes(indices, queries)
